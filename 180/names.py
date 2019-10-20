3fe69860c72fa50137c7a0db641715119e4fab84
boraxpr <naipawat.poo@student.mahidol.ac.th>
2019-10-20 22:36:58 +0700

    submission Bite 180 @ codechalleng.es

---
+++ 180/names.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    # you code
+    sorted(countries.items())
+    return countries
+
+
+# x = group_names_by_country(data)
+# print(x)
