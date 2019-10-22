a71400e3f92822052348084ed361827e69b77073
boraxpr <naipawat.poo@student.mahidol.ac.th>
2019-10-23 01:16:58 +0700

    bite 101

---
+++ 101/driving.py
@@ -0,0 +1,11 @@
+MIN_DRIVING_AGE = 18
+
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    if age > MIN_DRIVING_AGE:
+        return True
+    else:
+        return False
+
