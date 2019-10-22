ae0a34e01d2f7ea19d08d00b846141c023b586e1
boraxpr <naipawat.poo@student.mahidol.ac.th>
2019-10-23 01:30:38 +0700

    bite 102submit and 101bugfix

--- 101/driving.py
+++ 101/driving.py
@@ -4,9 +4,9 @@ MIN_DRIVING_AGE = 18
 def allowed_driving(name, age):
     """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
        checking the passed in age against the MIN_DRIVING_AGE constant"""
-    if age > MIN_DRIVING_AGE:
+    if age >= MIN_DRIVING_AGE:
         print(name + " is allowed to drive")
     else:
         print(name + " is not allowed to drive")
 
-allowed_driving("Tim", 29)
+# allowed_driving("Tim", 29)
---
+++ 102/colors.py
@@ -0,0 +1,28 @@
+VALID_COLORS = ['blue', 'yellow', 'red']
+
+
+def print_colors():
+    """In the while loop ask the user to enter a color,
+       lowercase it and store it in a variable. Next check: 
+       - if 'quit' was entered for color, print 'bye' and break. 
+       - if the color is not in VALID_COLORS, print 'Not a valid color' and continue.
+       - otherwise print the color in lower case."""
+    while True:
+        print("A Color: ")
+        inp = input()
+        input_lowered = inp.lower()
+        valid = 0
+        if input_lowered == "quit":
+            print("bye")
+            break
+        for color in VALID_COLORS:
+            if color == input_lowered:
+                valid = 1
+        if valid == 1:
+            print(input_lowered)
+        else:
+            print("Not a valid color")
+            continue
+        pass
+
+print_colors()
