ec7515a772f0694a52ca3d9644cd530ec141b142
boraxpr <naipawat.poo@student.mahidol.ac.th>
2019-10-23 01:19:31 +0700

    bite 101

--- 101/driving.py
+++ 101/driving.py
@@ -5,7 +5,8 @@ def allowed_driving(name, age):
     """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
        checking the passed in age against the MIN_DRIVING_AGE constant"""
     if age > MIN_DRIVING_AGE:
-        return True
+        print(name + " is allowed to drive")
     else:
-        return False
+        print(name + " is not allowed to drive")
 
+allowed_driving("Tim", 29)
