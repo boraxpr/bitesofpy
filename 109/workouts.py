05ca29b6760de2eb984b54eb3bb3aa7abf60fd73
boraxpr <naipawat.poo@student.mahidol.ac.th>
2019-10-28 12:44:35 +0700

    bite 109

---
+++ 109/workouts.py
@@ -0,0 +1,44 @@
+workout_schedule = {'Friday': 'Shoulders',
+                    'Monday': 'Chest+biceps',
+                    'Saturday': 'Rest',
+                    'Sunday': 'Rest',
+                    'Thursday': 'Legs',
+                    'Tuesday': 'Back+triceps',
+                    'Wednesday': 'Core'}
+rest, chill, go_train = 'Rest', 'Chill out!', 'Go train {}'
+
+
+def get_workout_motd(day):
+    """First title case the passed in day argument
+       (so monday or MONDAY both result in Monday).
+
+       Then check if this title cased day is in the given workout_schedule dict.
+
+       If it is retrieve the workout (value), if not raise a KeyError.
+
+       Return the chill variable if it's a rest day (Saturday / Sunday),
+       else return the go_train variable with the workout interpolated.
+
+       Examples:
+       - if day is Saturday -> return 'Chill out!'
+       - if day is Thursday -> return 'Go train Legs'
+       - if day is Sunday -> return 'Chill out!'
+       - if day is Monday -> return 'Go train Chest+biceps'
+
+       Trivia: /etc/motd is a file on Unix-like systems that contains
+       a 'message of the day'"""
+    day = day.title()
+    try:
+        value = workout_schedule.__getitem__(day)
+    except KeyError:
+        raise KeyError
+    # print(value)
+    if day is "Saturday" or "Sunday":
+        return go_train.format(value)
+    else:
+        return chill
+
+    pass
+
+# get_workout_motd("Saturday")
+# print(get_workout_motd("monday"))
