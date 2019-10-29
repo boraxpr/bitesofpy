51d9f8607a7856c0a353d5d943bc1a69390aa424
boraxpr <naipawat.poo@student.mahidol.ac.th>
2019-10-29 15:03:50 +0700

    bite 32

---
+++ 32/inventory.py
@@ -0,0 +1,11 @@
+import copy
+items = [{'id': 1, 'name': 'laptop', 'value': 1000},
+         {'id': 2, 'name': 'chair', 'value': 300},
+         {'id': 3, 'name': 'book', 'value': 20}]
+
+
+def duplicate_items(items):
+    items_copy = copy.deepcopy(items)
+    return items_copy[:]
+
+# duplicate_items(items)
