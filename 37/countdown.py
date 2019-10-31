fbbadea1e7c83b7eeacc3a3bd4a257fd64f350f1
boraxpr <naipawat.poo@student.mahidol.ac.th>
2019-11-01 00:16:42 +0700

    bite 37

--- 37/countdown.py
+++ 37/countdown.py
@@ -4,12 +4,13 @@ def countdown_for(start=10):
     print('time is up')
 
 
-def countdown_recursive(start=10):
+def countdown_recursive(start):
     if start == 1:
+        print(start)
         print('time is up')
         return 1
     else:
         print(start)
         return countdown_recursive(start-1)
 
-countdown_recursive()
+countdown_recursive(100)
