2ce3eb2b801c2be2ba5903ef33508a3b8af7248a
boraxpr <naipawat.poo@student.mahidol.ac.th>
2019-11-14 11:19:40 +0700

    bite 214

--- 214/countdown.py
+++ 214/countdown.py
@@ -2,11 +2,7 @@ def countdown():
     """Write a generator that counts from 100 to 1"""
     num = 100
     while True:
-            yield num
-            num -= 1
-            if num < 1 or num > 100:
-                raise StopIteration
-
-
-
-
+        yield num
+        num -= 1
+        if num < 1:
+            raise StopIteration
