ffc261428c949305f2129a594adfdde064aba90b
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2019-12-29 20:27:15 +0700

    bite 161 - T_T

--- 161/tree.py
+++ 161/tree.py
@@ -5,17 +5,16 @@ def count_dirs_and_files(directory='.'):
     """Count the amount of of directories and files in passed in "directory" arg.
        Return a tuple of (number_of_directories, number_of_files)
     """
-    number_of_files = 0
-    number_of_directories = 0
-    with os.scandir(directory) as it:
-        for entry in it:
-            if entry.is_file():
-                number_of_files = number_of_files + 1
-            elif entry.is_dir():
-                number_of_directories = number_of_directories + 1
-    counts = list([number_of_directories,number_of_files])
-    return tuple(counts)
+    files = []
+    dirs = []
+    for(dirpath, dirnames, filenames) in os.walk(directory):
+        for file in filenames:
+            files.append(file)
+        for dir in dirnames:
+            dirs.append(dir)
+    return dirs.__len__(), files.__len__()
 
 
 
-print(count_dirs_and_files())
+
+# print(count_dirs_and_files('project'))
