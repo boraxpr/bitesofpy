5a4bbfc98ccc2d808387ffc9915fc3b009cf6345
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2019-12-29 21:15:08 +0700

    bite 165 - bug fix

--- 165/passwd.py
+++ 165/passwd.py
@@ -1,5 +1,20 @@
 DEFAULT_SHELL = 'bash'
 # https://github.com/avar/git-anyonecanedit-etc/blob/master/passwd
+OTHER_PASSWD_OUTPUT = """root:!:0:0::/:/usr/bin/ksh
+daemon:!:1:1::/etc:
+bin:!:2:2::/bin:
+sys:!:3:3::/usr/sys:
+adm:!:4:4::/var/adm:
+uucp:!:5:5::/usr/lib/uucp:
+guest:!:100:100::/home/guest:
+nobody:!:4294967294:4294967294::/:
+lpd:!:9:4294967294::/:
+lp:*:11:11::/var/spool/lp:/bin/false
+invscout:*:200:1::/var/adm/invscout:/usr/bin/ksh
+nuucp:*:6:5:uucp login user:/var/spool/uucppublic:/usr/sbin/uucp/uucico
+paul:!:201:1::/home/paul:/usr/bin/ksh
+jdoe:*:202:1:John Doe:/home/jdoe:/usr/bin/ksh"""
+
 PASSWD_OUTPUT = """root:x:0:0:root:/root:/bin/bash
 daemon:x:1:1:daemon:/usr/sbin:/bin/sh
 bin:x:2:2:bin:/bin:/bin/sh
@@ -41,14 +56,15 @@ def get_users_for_shell(passwd_output: str = PASSWD_OUTPUT,
        Return a list of users.
     """
     users = []
-    for line in PASSWD_OUTPUT.splitlines():
+    for line in passwd_output.splitlines():
         # print(line)
         usr, x, uid, gid, room, homedir, program = line.split(":")
         program = program.split("/")
+        print(program)
         this_shell = program[-1]
         if this_shell == grep_shell:
-            print(this_shell)
             users.append(usr)
     return sorted(users)
 
-# print(get_users_for_shell())
+
+# print(get_users_for_shell(passwd_output=OTHER_PASSWD_OUTPUT, grep_shell='ksh'))
