81dd246279e2d00dbb27e69a6991f9bc057d0119
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2019-12-29 18:05:32 +0700

    bite 19

---
+++ 161/tree.py
@@ -0,0 +1,21 @@
+import os
+
+
+def count_dirs_and_files(directory='.'):
+    """Count the amount of of directories and files in passed in "directory" arg.
+       Return a tuple of (number_of_directories, number_of_files)
+    """
+    number_of_files = 0
+    number_of_directories = 0
+    with os.scandir(directory) as it:
+        for entry in it:
+            if entry.is_file():
+                number_of_files += 1
+    with os.scandir(directory) as it:
+        for entry in it:
+            if entry.is_dir():
+                number_of_directories += 1
+    t = (number_of_directories, number_of_files)
+    return t
+
+print(count_dirs_and_files())
