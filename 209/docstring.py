3270b0c48f024f17e289f29971406d4fda87e7e5
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-11 21:17:21 +0700

    bite 209

---
+++ 209/docstring.py
@@ -0,0 +1,9 @@
+def sum_numbers(numbers):
+    """Returns sum of numbers
+
+        :param numbers: Start index, defaults to 1
+        :type numbers: list
+        :return: sum of numbers
+        :rtype: int
+        """
+    pass
