9f47684d72040f2b61b77e7cba7714d628abc3ff
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-11 20:56:58 +0700

    bite 192 - improved

--- 192/log_it.py
+++ 192/log_it.py
@@ -1,26 +1,28 @@
 import logging
 from typing import Callable
-# print(callable(logging.info))
-DEBUG = logging.debug
-INFO = logging.info
-WARNING = logging.warning
-ERROR = logging.error
-CRITICAL = logging.critical
+
+pybites_logger = logging.getLogger("pybites_logger")
+DEBUG = pybites_logger.debug
+INFO = pybites_logger.info
+WARNING = pybites_logger.warning
+ERROR = pybites_logger.error
+CRITICAL = pybites_logger.critical
 
 
 def log_it(level: Callable, msg: str) -> None:
-    level_name = level.__name__
-    logger = logging.getLogger("pybites_logger")
-    if level_name == "debug":
-        logger.log(level=10, msg=msg)
-    elif level_name == "info":
-        logger.log(level=20, msg=msg)
-    elif level_name == "warning":
-        logger.log(level=30, msg=msg)
-    elif level_name == "error":
-        logger.log(level=40, msg=msg)
-    elif level_name == "critical":
-        logger.log(level=50, msg=msg)
+    level(msg)
+    # level_name = level.__name__
+    # logger = logging.getLogger("pybites_logger")
+    # if level_name == "debug":
+    #     logger.log(level=10, msg=msg)
+    # elif level_name == "info":
+    #     logger.log(level=20, msg=msg)
+    # elif level_name == "warning":
+    #     logger.log(level=30, msg=msg)
+    # elif level_name == "error":
+    #     logger.log(level=40, msg=msg)
+    # elif level_name == "critical":
+    #     logger.log(level=50, msg=msg)
 
 
 
