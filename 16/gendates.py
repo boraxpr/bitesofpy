9059c806435b77c92a3e24d8f79d121183426f46
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-12 20:51:56 +0700

    bites 16

--- 16/gendates.py
+++ 16/gendates.py
@@ -1,7 +1,24 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from itertools import islice
 
 PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-    pass
+    born = PYBITES_BORN
+    count = 0
+    while True:
+        if count != 3 and count != 8:
+            yield born + timedelta(days=100)
+            born += timedelta(days=100)
+        elif count == 3:
+            yield PYBITES_BORN + timedelta(days=365)
+        elif count == 8:
+            yield PYBITES_BORN + timedelta(days=365*2)
+        count += 1
+
+
+dates = list(islice(gen_special_pybites_dates(), 10))
+for date in dates:
+    print(date)
+# print(PYBITES_BORN+timedelta(days=365*2))
