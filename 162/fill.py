eefcc09ab0b652fcd2bd8469bcbf6bec9e3fe1d6
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-17 23:41:09 +0700

    bites 162 - remove irrelevant comment

--- 162/fill.py
+++ 162/fill.py
@@ -4,5 +4,3 @@ HTML_SPACE = '&nbsp;'
 def prefill_with_character(value, column_length=4, fill_char=HTML_SPACE):
     """Prepend value with fill_char for given column_length"""
     return (column_length-len(str(value)))*fill_char+str(value)
-
-# prefill_with_character("cxxx")
