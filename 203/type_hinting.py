4d4dbf5df72b50c13c1248645bdb5b7e34566795
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-18 00:10:18 +0700

    bites 203 - add type hinting for returns

--- 203/type_hinting.py
+++ 203/type_hinting.py
@@ -1,7 +1,4 @@
-from dataclasses import dataclass
-
-
-@dataclass
+import typing
 class Employee:
     """Simple Employee class
 
@@ -21,7 +18,7 @@ class Employee:
         self.hours_per_day = hours_per_day
         self.wage = wage
 
-    def _rounder(self, number:float, places:int):
+    def _rounder(self, number:float, places:int) -> str:
         """Rounds a number the specified number of places
 
         :param number: Float of number of round
@@ -32,7 +29,7 @@ class Employee:
         return f"${amount:0.2f}"
 
     @property
-    def weekly_pay(self):
+    def weekly_pay(self) -> str:
         """Returns amount of weekly pay in US currency
 
         For instance: $250.75
