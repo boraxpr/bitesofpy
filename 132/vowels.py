a9fbe49cf59a43d051e71659de3755c70a7a2fa9
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-24 12:57:29 +0700

    bites 132 - refactored

--- 132/vowels.py
+++ 132/vowels.py
@@ -1,5 +1,5 @@
 VOWELS = list('aeiou')
-text="""Python is an easy to learn, powerful programming language."
+text = """Python is an easy to learn, powerful programming language."
      "It has efficient high-level data structures and a simple "
      "but effective approach to object-oriented programming. "
      "Python’s elegant syntax and dynamic typing, together with "
@@ -7,26 +7,19 @@ text="""Python is an easy to learn, powerful programming language."
      "scripting and rapid application development in many areas "
      "on most platforms."""
 
+
+def count_vowels(word):
+    return len([v for v in word if v in VOWELS])
+
+
 def get_word_max_vowels(text):
     """Get the case insensitive word in text that has most vowels.
        Return a tuple of the matching word and the vowel count, e.g.
        ('object-oriented', 6)"""
     # print(type(text))
-    words = []
-    for line in text.splitlines():
-        line = [word.lower() for word in line.split(" ") if len(word) > 0]
-        words.append(line)
-    listofwords = []
-    for list in words:
-        listofwords += list
-    result = []
-    for word in listofwords:
-        point = 0
-        for ch in word:
-            if ch in VOWELS:
-                point+=1
-        result.append((word,point))
-    result.sort(key=lambda x:x[1])
-    return result[-1]
+    words = text.lower().split(" ")
+    words = [(word, count_vowels(word)) for word in words]
+    return max(words,key=lambda x: x[1])
+
 
 print(get_word_max_vowels(text))
