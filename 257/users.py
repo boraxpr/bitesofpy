f09f03c8223c2884c053f6c27605dc66b1b9a86b
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-25 21:26:17 +0700

    bites 257

---
+++ 257/users.py
@@ -0,0 +1,35 @@
+pw4 = """
+mysql:x:106:107:MySQL Server,,,:/var/lib/mysql:/bin/false
+avar:x:1000:1000::/home/avar:/bin/bash
+chad:x:1001:1001::/home/chad:/bin/bash
+git-svn-mirror:x:1002:1002:Git mirror,,,:/home/git-svn-mirror:/bin/bash
+gerrit2:x:1003:1003:Gerrit User,,,:/home/gerrit2:/bin/bash
+avahi:x:107:108:Avahi mDNS daemon,,,:/var/run/avahi-daemon:/bin/false
+postfix:x:108:112::/var/spool/postfix:/bin/false
+ssh-rsa:x:1004:1004::/home/ssh-rsa:/bin/bash
+artagnon:x:1005:1005:Ramkumar R,,,,Git GSOC:/home/artagnon:/bin/bash
+"""
+
+
+def get_users(passwd: str) -> dict:
+    """Split password output by newline,
+      extract user and name (1st and 5th columns),
+      strip trailing commas from name,
+      replace multiple commas in name with a single space
+      return dict of keys = user, values = name.
+    """
+    username = dict()
+    passwd = passwd.splitlines()
+    for entry in passwd:
+        if len(entry) > 1:
+            en = entry.split(':')
+            if len(en[4]) != 0:
+                name = en[4].replace(',', ' ').split()
+                name = " ".join(name)
+                username[en[0]] = name
+            else:
+                username[en[0]] = 'unknown'
+    return username
+
+
+print(get_users(pw4))
