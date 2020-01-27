dca708a74d08028a29b6321ab947f5bf2414c5c8
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-27 12:58:15 +0700

    bites 146 - refactor

--- 146/rhombus.py
+++ 146/rhombus.py
@@ -17,14 +17,8 @@ def gen_rhombus(width):
          ***
           *
     """
-
-    for rowz in range(1, (width * 2) + 1, 2):
-        if rowz-width <= 0:
-            yield '{:^{width}}'.format(STAR * rowz, width=width)
-        else:
-            multiplierOf4 = (rowz-width)//2
-            yield '{:^{width}}'.format(STAR * (rowz-4*multiplierOf4), width=width)
-
-gen = gen_rhombus(5)
-for row in gen:
-    print(row)
+    print_row = lambda i,width: f'{STAR*i: ^{width}}'
+    for i in range(1, width+1, 2):
+        yield print_row(i, width)
+    for i in range(width-2, 0, -2):
+        yield print_row(i, width)
