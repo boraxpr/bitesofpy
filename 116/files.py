2921045af3223b1a72e37d933a5a3fc05e716c88
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-29 16:54:40 +0700

    bites 116 - os and glob - fix glob

--- 116/files.py
+++ 116/files.py
@@ -7,4 +7,4 @@ ONE_KB = 1024
 def get_files(dirname, size_in_kb):
     """Return files in dirname that are >= size_in_kb"""
     glob_pattern = os.path.join(dirname, '*')
-    return [file for file in glob(dirname+"*") if os.path.getsize(file) >= ONE_KB*size_in_kb]
+    return [file for file in glob(glob_pattern) if os.path.getsize(file) >= ONE_KB*size_in_kb]
