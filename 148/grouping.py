09d04c78723c676136bc41405a12f93044d75282
naipawat.poo@student.mahidol.ac.th <naipawat.poo@student.mahidol.ac.th>
2020-01-31 10:12:25 +0700

    bites 148 - grouping

---
+++ 148/grouping.py
@@ -0,0 +1,34 @@
+from itertools import groupby
+
+cars = [
+    # need mock data? -> https://www.mockaroo.com == awesome
+    ('Mercedes-Benz', '300D'), ('Mercedes-Benz', '600SEL'),
+    ('Toyota', 'Avalon'), ('Ford', 'Bronco'),
+    ('Chevrolet', 'Cavalier'), ('Chevrolet', 'Corvette'),
+    ('Mercedes-Benz', 'E-Class'), ('Hyundai', 'Elantra'),
+    ('Volkswagen', 'GTI'), ('Toyota', 'Highlander'),
+    ('Chevrolet', 'Impala'), ('Nissan', 'Maxima'),
+    ('Ford', 'Mustang'), ('Kia', 'Optima'),
+    ('Volkswagen', 'Passat'), ('Nissan', 'Pathfinder'),
+    ('Volkswagen', 'Routan'), ('Hyundai', 'Sonata'),
+    ('Kia', 'Sorento'), ('Kia', 'Sportage'),
+    ('Ford', 'Taurus'), ('Nissan', 'Titan'),
+    ('Toyota', 'Tundra'), ('Hyundai', 'Veracruz'),
+]
+
+
+def group_cars_by_manufacturer(cars):
+    """Iterate though the list of (manufacturer, model) tuples
+       of the cars list defined above and generate the output as described
+       in the Bite description (see the tests for the full output).
+       
+       No return here, just print to the console. We use pytest > capfd to
+       validate your output :)
+    """
+    cars = sorted(cars, key=lambda x: x[0])
+    for key, group in groupby(cars, lambda x: x[0]):
+        cars_text = "\n- ".join(list(map(lambda x: x[1], group)))
+        print(key.upper() + "\n- " + cars_text + "\n")
+
+
+# group_cars_by_manufacturer(cars)
