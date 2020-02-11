0108926b465f7db7a42e0f93c643ae879aecbfc4
boraxpr <naipawat.poo@student.mahidol.ac.th>
2020-02-11 22:42:42 +0700

    bites 81 - namedtuple sorting - refactor (using join instead of logics)

--- 44/license.py
+++ 44/license.py
@@ -3,17 +3,15 @@ import string
 
 
 def gen_key(parts=4, chars_per_part=8):
-    key = ""
+    key = []
     for part in range(parts):
-        # print(part.__str__() + " " + parts.__str__())
         p = ""
         for char in range(chars_per_part):
             c = secrets.choice(string.ascii_uppercase+string.digits)
             p = p + c
-        if part != parts-1:
-            p = p + "-"
-        key = key + p
-    return key
+        key.append(p)
+
+    return "-".join(key)
 
 
 print(gen_key())
