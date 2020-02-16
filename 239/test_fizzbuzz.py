1fede10740629727727c7b59986f8061f03c1dea
boraxpr <naipawat.poo@student.mahidol.ac.th>
2020-02-16 15:46:27 +0700

    bites 239 - pytest parametrizing - add a case for not related to mod 3 and mod 5

--- 239/test_fizzbuzz.py
+++ 239/test_fizzbuzz.py
@@ -7,7 +7,7 @@ from fizzbuzz import fizzbuzz
     [(0, "Fizz Buzz"),(15,"Fizz Buzz"), (45,"Fizz Buzz"),
      (3, "Fizz"), (9, "Fizz"), (12, "Fizz"),
      (5, "Buzz"), (10, "Buzz"), (20, "Buzz"),
-     (25, "Buzz"), (35, "Buzz")]
+     (25, "Buzz"), (35, "Buzz"), (2, 2)]
 )
 def test_fizzbuzz(test_input, expected):
     assert fizzbuzz(test_input) == expected
