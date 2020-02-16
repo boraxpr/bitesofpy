747596e813645af0eb4379c1eb0cf2b5e0402ce3
boraxpr <naipawat.poo@student.mahidol.ac.th>
2020-02-16 15:35:51 +0700

    bites 239 - pytest parametrizing

---
+++ 239/test_fizzbuzz.py
@@ -0,0 +1,12 @@
+import pytest
+from fizzbuzz import fizzbuzz
+
+# write one or more pytest functions below, they need to start with test_
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [(15,"Fizz Buzz"), (45,"Fizz Buzz"),
+     (3, "Fizz"), (9, "Fizz"), (12, "Fizz"),
+     (5, "Buzz"), (10, "Buzz"), (20, "Buzz")]
+)
+def test_fizzbuzz(test_input, expected):
+    assert fizzbuzz(test_input) == expected
