fefaecb27a0e044af13c0d8d6de03070ff7fa8f3
boraxpr <naipawat.poo@student.mahidol.ac.th>
2020-02-19 21:55:30 +0700

    bites 147 - Refactor: Using dateutil.rrule instead of dateutil.relativedelta

--- 147/hundred_days.py
+++ 147/hundred_days.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from dateutil.relativedelta import relativedelta
+from dateutil.rrule import DAILY, rrule, MO, TU, WE, TH, FR
 
 TODAY = date(year=2018, month=11, day=29)
 
@@ -9,12 +9,10 @@ def get_hundred_weekdays(start_date=TODAY):
     """Return a list of hundred date objects starting from
        start_date up till 100 weekdays later, so +100 days
        skipping Saturdays and Sundays"""
-    hundred_weekdays = list()
-    while len(hundred_weekdays) < 100:
-        if start_date.weekday() not in [5, 6]:
-            hundred_weekdays.append(start_date)
-        start_date += relativedelta(days=1)
-    return hundred_weekdays
+    date_range = rrule(DAILY, count=100,
+                       byweekday=(MO, TU, WE, TH, FR),
+                       dtstart=start_date)
+    return [dt.date() for dt in date_range]
 
 
 
