b04e580d2c96d1fc5e590c7b96f75edc6a2ee578
boraxpr <naipawat.poo@student.mahidol.ac.th>
2020-02-24 22:21:43 +0700

    bite 230 - Operator Overloading - Refactored : make the code shorter by implement inline if "expression"s.

--- 230/thumbs.py
+++ 230/thumbs.py
@@ -4,14 +4,9 @@ THUMBS_UP, THUMBS_DOWN = '👍', '👎'
 def emoji_multiply(multiplier):
     if multiplier == 0:
         raise ValueError("Specify a number")
-    thumb = THUMBS_UP
-    if multiplier < 0:
-        thumb = THUMBS_DOWN
+    thumb = THUMBS_UP if multiplier > 0 else THUMBS_DOWN
     multiplier = abs(multiplier)
-    if multiplier in [1, 2, 3]:
-        return thumb * multiplier
-    else:
-        return str(thumb) + " ({}x)".format(multiplier)
+    return f"{thumb} ({multiplier}x)" if multiplier > 3 else thumb*multiplier
 
 
 class Thumbs:
@@ -19,4 +14,4 @@ class Thumbs:
         return emoji_multiply(multiplier)
 
     def __rmul__(self, multiplier):
-        return emoji_multiply(multiplier)
+        return self.__mul__(multiplier)
